b0e325507aeb98bd47686b03e9de85358763a5a6
Mike Solomon <mikesol@hey.com>
2021-02-19 12:29:03 -0800

    Partial checkin for exercise 1

--- src/index.exercise.js
+++ src/index.exercise.js
@@ -1,9 +1,55 @@
-// 🐨 you'll need to import React and ReactDOM up here
+import React from 'react'
+import ReactDOM from 'react-dom'
+import {Logo} from './components/logo'
 
-// 🐨 you'll also need to import the Logo component from './components/logo'
+import {Dialog} from '@reach/dialog'
+import '@reach/dialog/styles.css'
 
-// 🐨 create an App component here and render the logo, the title ("Bookshelf"), a login button, and a register button.
-// 🐨 for fun, you can add event handlers for both buttons to alert that the button was clicked
+function App() {
+  const [openModal, setOpenModal] = React.useState('none')
 
-// 🐨 use ReactDOM to render the <App /> to the root element
-// 💰 find the root element with: document.getElementById('root')
+  const openLoginDialog = () => setOpenModal('login')
+  const closeLoginDialog = () => setOpenModal('none')
+
+  const openRegisterDialog = () => setOpenModal('register')
+  const closeRegisterDialog = () => setOpenModal('none')
+
+  return (
+    <div>
+      <Logo width="80" height="80" />
+      <h1>Bookshelf</h1>
+
+      <div>
+        <button onClick={openLoginDialog}>Login</button>
+      </div>
+
+      <div>
+        <button onClick={openRegisterDialog}>Register</button>
+      </div>
+
+      <Dialog
+        aria-label="Login form"
+        isOpen={openModal === 'login'}
+        onDismiss={closeLoginDialog}
+      >
+        <div>
+          <button onClick={closeLoginDialog}>Close</button>
+        </div>
+
+        <h3>Hello there login</h3>
+      </Dialog>
+
+      <Dialog
+        aria-label="Registration form"
+        isOpen={openModal === 'register'}
+        onDismiss={closeRegisterDialog}
+      >
+        <button onClick={closeRegisterDialog}>Close</button>
+
+        <h3>Hello there register</h3>
+      </Dialog>
+    </div>
+  )
+}
+
+ReactDOM.render(<App />, document.getElementById('root'))
